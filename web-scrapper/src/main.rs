334a761239bcae1e0d1ac9a0ddd00ec547d58070
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-11-06 23:59:10 +0600

    web scrapper done

--- web-scrapper/src/main.rs
+++ web-scrapper/src/main.rs
@@ -1,8 +1,10 @@
+use std::{collections::{HashSet, VecDeque}, io::{Write, stdin, stdout}};
+
 use reqwest;
 use scraper::{Html, Selector};
 
 
-fn scrapper(url: &str) -> Option<String>{
+fn scraper(url: &str) -> Option<String>{
     match reqwest::blocking::get(url) {
         Ok(response) => {
             if response.status().is_success() {
@@ -50,16 +52,60 @@ fn extract_links(html: &str, base_url: &str) -> Vec<String> {
 }
 
 fn main() {
-    let website_url = String::from("https://webscraper.io/test-sites/e-commerce/static");
+    // let website_url = String::from("https://webscraper.io/test-sites/e-commerce/static");
+    // let max_depth = 2; 
+
+    let mut websit_url_str = String::new();
+    let mut depth_str = String::new();
+    print!("Enter website url: ");
+    stdout().flush().unwrap();
+    stdin().read_line(&mut websit_url_str).unwrap();
+    let website_url = websit_url_str.trim();
+
+    print!("Enter depth: ");
+    stdout().flush().unwrap();
+    stdin().read_line(&mut depth_str).unwrap();
+    let max_depth = depth_str.trim().parse().expect("need a Integer");
+    
+
+
     println!("Scrapping webpage(recussive web-page): {}", website_url);
 
-    if let Some(html) = scrapper(website_url.as_str()) {
-        println!("Html length:{} bytes", html.len());
-        let links = extract_links(html.as_str(), website_url.as_str());
-        for link in links{
-            println!("link: {}",link);
+    let mut scrapped = HashSet::new();
+    let mut failed_scraped = HashSet::new();
+
+    let mut queue = VecDeque::new();
+    queue.push_back((website_url.to_string(), 0));
+
+    while let Some((current_url, depth)) = queue.pop_front() {
+        if depth > max_depth {
+            continue;
         }
-    } else {
-        println!("No content fetched")
+        if scrapped.contains(&current_url) {
+            continue;
+        }
+        println!("Depth: {} | Scrapping: {}", depth, current_url);
+        match scraper(&current_url) {
+            Some(html) => {
+                println!("😃😃😃 fetched {} bytes", html.len());
+                if depth < max_depth {
+                    let links = extract_links(&html, &current_url);
+                    for link in links {
+                        if !scrapped.contains(link.as_str()) && !failed_scraped.contains(link.as_str()) {
+                            queue.push_back((link.clone(), depth + 1));
+                        }
+                    }
+                }
+            }
+            None => {
+                eprintln!("😰😰😰 Failed to fetch: {}", current_url);
+                failed_scraped.insert(current_url.clone());
+            }
+        }
+        scrapped.insert(current_url.clone());
     }
+
+    println!("\t\tScrapping completed. \ntotal succefully of scrapped url count: {}", scrapped.len());
+    println!("total failed scrapped url count: {}", failed_scraped.len());
+    
 }
