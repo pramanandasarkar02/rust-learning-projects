066c9145d90fe01c12cfddd8c9c57c25cec923c4
Pramananda Sarkar <pramanandasarkar02@gmail.com>
2025-11-06 07:28:23 +0600

    scrapper basic version done

--- web-scrapper/src/main.rs
+++ web-scrapper/src/main.rs
@@ -1,13 +1,13 @@
 use reqwest;
+use scraper::{Html, Selector};
 
 
-fn scrapper(url: String) -> Option<String>{
+fn scrapper(url: &str) -> Option<String>{
     match reqwest::blocking::get(url) {
         Ok(response) => {
             if response.status().is_success() {
                 match response.text() {
                     Ok(body) => {
-                        println!("Content fetched {}", body);
                         Some(body)
                     }
                     Err(e) => {
@@ -28,12 +28,37 @@ fn scrapper(url: String) -> Option<String>{
 }
 
 
+
+fn extract_links(html: &str, base_url: &str) -> Vec<String> {
+    let document = Html::parse_document(html);
+    let selector = Selector::parse("a").unwrap();
+
+    document
+        .select(&selector)
+        .filter_map(|element|element.value().attr("href"))
+        .map(|href| {
+            if href.starts_with("http") {
+                href.to_string()
+            } else if href.starts_with('/') {
+                format!("{}{}", base_url.trim_end_matches('/'), href)
+            } else {
+                format!("{}/{}", base_url.trim_end_matches('/'), href)
+            }
+        })
+        .collect()
+        
+}
+
 fn main() {
     let website_url = String::from("https://webscraper.io/test-sites/e-commerce/static");
     println!("Scrapping webpage(recussive web-page): {}", website_url);
 
-    if let Some(html) = scrapper(website_url) {
-        println!("Html length:{} bytes", html.len())
+    if let Some(html) = scrapper(website_url.as_str()) {
+        println!("Html length:{} bytes", html.len());
+        let links = extract_links(html.as_str(), website_url.as_str());
+        for link in links{
+            println!("link: {}",link);
+        }
     } else {
         println!("No content fetched")
     }
